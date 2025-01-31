0632e425bd7828786713770949f9c8fb46ca2259
over-Master <kassiolivier91@gmail.com>
2025-01-31 19:48:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,485 @@
+// console.log("Bonsoir");
+// console.log(2)
+// let nomDeLaPersonne="paul"
+// console.log(nomDeLaPersonne);
+
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(4-2);
+// console.log(5%2)
+// let n;
+// n=2+2;
+// console.log(n);
+
+// // concaténation
+// console.log ("bonjour"+ "bonsoir");
+// console.log ("bonjour + 2")
+
+// let a;
+// a= 20;
+// a+=10;
+// console.log(a)
+
+
+// console.log(2<1);
+// console.log(1<2);
+// console.log(5<=2);
+// console.log(5>3);
+// console.log(5==5);
+// console.log(4==5);
+// console.log(5!=5);
+// console.log("10"==10);
+// console.log("10"===10);
+
+// console.log(1==1 && 2==3);
+
+// console.log (1==1 || 2==3);
+
+
+// let b=2>4;
+// console.log (b);
+// console.log(b || false);
+
+// console.log(!true)
+// console.log (!false)
+// console.log(!b)
+
+// console.log("texte1");
+// console.log("texte2");
+// console.log("texte3");
+// console.log("texte4");
+
+// // let age=19;
+// // if (age<18) {//si
+// // //     console.log ("vous n'avez pas")
+// // // }
+
+// // // else if( age<20){//sinon si
+// // //     console.log("un peu jeune")
+
+// // //      }
+
+// // //      else{
+// // //         console.log("tout est faux")
+// // //      }
+// // //      //alert("bonjour");
+// // //      //console.log(5);
+// // // let reponse=confirm ("voulez vous un café?")
+// // // console.log(reponse);
+// // // if(reponse==true){
+// // //     console.log("votre café est servi");
+// // // }
+// // // else{
+// // //     console.log("Tant pis")
+// // // }
+
+// let reponse = prompt("Entrez votre nom");
+//    console.log(reponse);
+
+// let b=23
+// while (b<534){
+//    console.log(b);
+   
+//    b=b+1
+//    }
+   
+
+// let a= prompt ("Quelle boisson voulez-vous?")
+// if (a=="café") {
+//  prompt ("combien de sucre") }
+   
+//  else if (a=="Soda") {
+//    prompt("Fanta ou Coca")
+//  }
+//  else if (a== "eau"){
+//    prompt("Fraiche?")}
+
+//  else if (a=="rien") {
+//    prompt("Voulez-vous manger")
+// //  }
+// let x=5
+// let y=100
+// function addition (x,y){
+//   let resultat =x+y;
+//   return resultat;
+//  }
+// let a= addition(x,y)
+// console.log(a)
+// let r= addition(2,3);
+// console.log(r);
+
+// console.log(addition(7,5));
+
+// function plusgrand (a,b) {
+  
+//   if (a>b){
+//     return a
+  
+//   }
+//   else{
+// //     return b
+// //   }
+
+// // }
+// // console.log(plusgrand(30,52))
+
+// // let personne ={
+// //  age:33,
+// //  nom: "hermann",
+// //  année:2024,
+// //  profession:"médecin",
+// // }
+// // console.log(personne);
+
+// // let age2=30
+// // let nom2= "odile"
+// // let année2=2000
+// // let profession2 = "conseillère"
+// // function verifierPremier(n){
+// //     if(n==0 || n==1){
+// //         return false;
+// //     }
+// //     else{
+// //         let i=2;
+// //         while(i<n){
+// //             if (n%i == 0)
+// //                 return false;
+// //         }
+// //         n = n+1;
+// //     }
+// //     return true
+// // }
+
+// // // console.log(verifierPremier(6))
+
+// // function premier(n){
+// //     if(n==0 || n==1);
+// //     n<10001
+// // }
+
+
+// // let personne ={
+// //      age:33,
+// //      nom: "hermann",
+// //      année:2024,
+// //      profession:"médecin",
+// //     }
+// //     console.log(personne);
+    
+// //     let age2=30
+// //     let nom2= "odile"
+// //     let année2=2000
+// //     let profession2 = "conseillère"
+// //     function verifierPremier(n) {
+// //         if(n==0 || n==1){
+// //             return false;
+// //         }
+
+
+// // let e1 = {
+// //     nom:"koffi",
+// //     prenoms:"kouakou",
+// //     classe:"1ereD"
+// // }
+// // let e2= {
+// //     nom:"kouakou",
+// //     prenoms:"kouadio",
+// //     classe:"3eme"
+// // }
+// // let e3= {
+// //    nom:"kouamé",
+// //    prenoms:"kouassi",
+// // // classe:"terminale"
+// // // }
+
+// // // console.log(e1)
+
+
+// // // let fruits=[
+// // // //     "Mangue","Orange", "Ananas"
+// // // ];
+// // // console.log(fruits[1]);
+
+// // let l=["hello","2","3" ,"bonjour",];
+
+// // console.log(l[l.lenght-1]);
+
+
+// // let l3=[2,[ [ 8,8,9 ] ] ] ;
+// // console.log(l3[1][0][0]);
+
+// // let l3=[5,8,10,20,9];
+// // let i=0;
+// // while(i<l3.length){
+// //     console.log(13[i]);
+// //     i=i+1;
+// // }
+
+
+// // for(let k=0; k<l3.length; k=k+1){
+// //     console.log(13[k]);
+// // }
+
+// // let k=l3.length-1
+// // while(k>=0){
+// //     console.log(l3[k]);
+// //     k=k - 1;
+
+// // }
+
+
+// // // faire +100 à chaque élément
+
+
+// // let j=0
+// // while(j<l3.length){
+// //     l3[j]=l3[j]+100;
+// //     j=j+1
+// // }
+// // console.log(l3);
+
+// // creer une liste de 6 éléments
+// // let over = ["olivier", "kassi" , "petit","Jesus", "paix", "amour", "joie"]
+
+// // // afficher le premier Element
+// // console.log(over[0]);
+// // // afficher le dernier élément
+// // console.log(over[over.length-1]);
+
+
+// // // rétirer 2 éléments
+// // over.pop();
+// // over.pop();
+
+// // // ajouter un nouvel élément
+// // over.push("oli")
+
+// // console.log(over)
+
+// // // afficher chaque element de ma liste
+
+// // let i = 0;
+// // while (i<this.length){
+// //     console.log(t[i]);
+
+// // }
+
+
+// // // declaration
+// // function calculerAge(anneeNaiss){
+// //     let resultat = (2025-anneeNaiss);
+// //     return resultat;
+
+// // }
+
+// // // appel
+// // let a=calculerAge(1990)
+// // console.log(a)
+
+
+// // // addition
+// // function addition(a,b){
+// //     let resultat  = a + b;
+// //     return resultat;
+// // }
+
+// function afficher5foisBonjour(){
+//     for(let i=0; i <5 ;i++){
+//         console.log("bonjour"+i)
+//     }
+// }
+// afficher5foisBonjour();
+
+
+
+
+// // nombres premiers 
+// // entre 0 et 1000
+// // 0 et 1 pas premiers
+// // premiers : diviseur 1 et n
+
+// function estPremier(n){
+//     if (n == 0 || n == 1){
+//         return false;
+//     }
+//     else{
+//         for(let i=2; i<n ; i++){
+//             if (n%i == 0){
+//                 return false
+
+//             }
+//         }
+//         return true
+//     }
+// }
+// for (let i =1; i< 1000; i++){
+//     let r =estPremier(i);
+//     if(r==true){
+//         console.log(i +"est premier")
+//     }
+//     else{
+//         console.log(i +"n'est pas premier")
+//     }
+// }
+
+
+// // liste
+
+// let l = [2, "sefeggeg", false];
+// console.log(l);
+// console.log (l[0]);
+// l[0]=5;
+
+// console.log(l)
+
+// // parcourir une liste 
+
+// for (let i = 0; i<= l.length-1; i++){
+//     console.log(l[i])
+// }
+
+// let l =[2,8,7,2,3];
+
+
+// function addition(x,y){
+//     returnx+y
+// }
+ 
+// const afficherBonjour = () => {
+//     console.log("BONJOUR");
+// }
+// setTimeout(() => {
+// console.log("BONJOUR");}, 2000)
+
+//  setInterval(() =>{
+//     console.log("BBONJOUR");
+//  }, 1000)
+
+
+// ;
+// // declaration d'une fonction fléchi
+// const multiplication= (a,b)=>{
+//     return a * b;
+// }
+
+// flitrer la liste
+
+// // function garderOupas(e){
+
+// // //     return! (e == 2);
+// //     }
+
+// let l=[2, 4, 4, 6, 2];
+// let l2= l.filter((e) => {
+// //     return! (e == 2 )
+// // });
+// // console.log(l2);
+
+// console.log (document)
+// let div1 =document.getElementById("Hola");
+
+// console.log(div1);
+// const divs = document.getElementsByTagName("div");
+// console.log (divs)
+
+// MODIFIER DU CSS AVEC JAVA SCRIPT
+
+//   1) selectionner
+// const btn =document.querySelector(".b");
+
+    //   2) Modifier
+
+//     function changerClass(){
+//         if(btn.getAttribute("class") == "b"){
+//             btn.setAttribute("class", "r");
+//         }
+//         else{
+//             btn.setAttribute("class", "b");
+//         }
+        
+        
+//     }
+
+//     setInterval(changerClass,200);
+
+
+//     const h= document.getElementById("Hola");
+
+
+//     console.log(h.innerText);
+//     console.log(h.innerHTML);
+
+//     // MODIFIER LE CONTENU
+
+//     h.innerText="BONSOIR"
+
+
+
+// // CREER UNE BALISE
+
+// const titre =document.createElement("h1")
+
+// //MODIFIER LA BALISE
+
+// titre.innerText="Ceci est le titre"
+
+// // AJOUTER A LA PAGE
+
+// const body = document.querySelector("body");
+// body.appendChild (titre)
+
+
+// let texteDiv = "Ceci est une div";
+
+// let contenu = `<div class='c1'>${texteDiv}</div>`;
+
+// // body.innerHTML=body.innerHTML + contenu;
+
+// body.classlist.add("d1");
+// console.log(body.classList);
+
+
+// RENDRE LA PAG DYNAMIQUE QUAND TU CLICK SUR UN BOUTTON
+// const button = document.querySelector(".b");
+// function f(){
+//     let h = document.getElementById("Hola");
+// h.innerHTML= h.innerHTML + "<div> Vous avez cliqué</div>";
+
+// }
+// button.addEventListener("click", f)
+
+
+//////////////////////////////////
+const Brouge= document.getElementById("red")
+const Bbleu= document.getElementById("blue")
+
+Brouge.addEventListener("mouseenter", ()=>{
+    const body = document.querySelector("body");
+    body.classList.remove("b");
+    body.classList.add("r");
+});
+
+Bbleu.addEventListener("mouseenter", ()=>{
+    const body = document.querySelector("body");
+    body.classList.remove("r");
+    body.classList.add("b");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
